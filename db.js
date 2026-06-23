9d3c2c4f58af5f965b59d1a32076f4a638b633f4
YAMS947 <yams947nz@gmail.com>
2026-06-23 12:33:35 -0600

    Elimine los elementos fuera de db.js y server.js, para permitir un trabajo limpio sobre esta versión

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2'); // Importamos la librería MYSQL2
 
+/* 
+    Crear conexión a la base de datos 
+    Es la que se usara durante todo el proyecto para manejar los datos
+*/
 const db = mysql.createConnection({
-    host: 'localhost',
-    port: 3307, // ← gracias al túnel SSH
-    user: 'innova-sys',
-    password: 'jam32**21las',
-    database: 'actividades_extracurriculares'
+    host: 'localhost', // El host al que conectamos gracias al tunel de host que abrimos
+    port: 3307, // Puerto que usa el tunel 
+    user: 'innova-sys', // Nombre del usuario que creamos para manipular la base de datos
+    password: 'jam32**21las', // La contraseña del usuario
+    database: 'actividades_extracurriculares' // Base de datos con la que trabajamos
 });
 
+/* Comprobamos que la conexión sea exitosa */
 db.connect(err => {
     if (err) {
         console.error('Error al conectar a MySQL:', err);
@@ -16,4 +21,5 @@ db.connect(err => {
     console.log('Conexión a MySQL exitosa desde VS Code');
 });
 
+/* Exportamos la conexión para usarla en otros archivos como "db" */
 module.exports = db;
