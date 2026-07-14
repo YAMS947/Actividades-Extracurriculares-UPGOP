cda0883aeb3a2f9ba4575e5693ab435d081e0cad
YAMS947 <yams947nz@gmail.com>
2026-07-13 23:58:44 -0600

    Ya se encuentra listo para empezar a trabajar, se corrigio el error

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ app.get('/', (req, res) => {
     y .js donde accedemos a la base de datos con su conexión a este mismo .js
 */
 //       (Referencia a usar,     Ruta donde se encuentra la logíca para manejar los datos)
-app.use(' /alumnos' , require('./routes/alumnos')); // Tabla de alumnos
-app.use(' /' , require('.routes/')); //
+
 
 // ===============================
 //  INICIAR SERVIDOR
