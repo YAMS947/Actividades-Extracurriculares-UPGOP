b82f060321f97171d3b6bd846dcee79b728c8ecc
YAMS947 <yams947nz@gmail.com>
2026-08-01 14:16:37 -0600

    Se añadio en todas las páginas el encabezado y pie de página ya funcional, se esperan cambios, pero nada mas en los archivos globales, por ende no afecta en el contenido que se añadio a todas las páginas. Además se resolvieron errores para la ejcución de la página con Node

--- routes/Gestion_De_Alumno.js
+++ routes/Gestion_De_Alumno.js
@@ -11,7 +11,7 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('../db'); // Ajusta la ruta según tu proyecto
+const db = require('../db'); 
 
 // ============================================================
 // 1. OBTENER DATOS DE UN ALUMNO + CARRERAS DISPONIBLES
